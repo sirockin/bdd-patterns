18a8b7061323b8d0d8b0f290b5d82537ec03f7dc
Dave Sirockin <dave@flow-systems.net>
2022-10-07 14:22:28 +0200

    steps are now methods of accountFeature

--- main_test.go
+++ main_test.go
@@ -1,58 +1,69 @@
 package main
 
 import (
+	"context"
+
 	"github.com/cucumber/godog"
 )
 
-func personHasCreatedAnAccount(name string) error {
+type accountFeature struct {
+}
+
+func (a *accountFeature) personHasCreatedAnAccount(name string) error {
 	return godog.ErrPending
 }
 
-func personHasSignedUp(name string) error {
+func (a *accountFeature) personHasSignedUp(name string) error {
 	return godog.ErrPending
 }
 
-func personShouldNotBeAuthenticated(name string) error {
+func (a *accountFeature) personShouldNotBeAuthenticated(name string) error {
 	return godog.ErrPending
 }
 
-func personShouldNotSeeAnyProjects(name string) error {
+func (a *accountFeature) personShouldNotSeeAnyProjects(name string) error {
 	return godog.ErrPending
 }
 
-func personShouldSeeAnErrorTellingThemToActivateTheAccount(name string) error {
+func (a *accountFeature) personShouldSeeAnErrorTellingThemToActivateTheAccount(name string) error {
 	return godog.ErrPending
 }
 
-func personTriesToSignIn(name string) error {
+func (a *accountFeature) personTriesToSignIn(name string) error {
 	return godog.ErrPending
 }
 
-func personCreatesAProject(name string) error {
+func (a *accountFeature) personCreatesAProject(name string) error {
 	return godog.ErrPending
 }
 
-func personShouldSeeTheirProject(name string) error {
+func (a *accountFeature) personShouldSeeTheirProject(name string) error {
 	return godog.ErrPending
 }
 
-func personActivatesTheirAccount(name string) error {
+func (a *accountFeature) personActivatesTheirAccount(name string) error {
 	return godog.ErrPending
 }
 
-func personShouldBeAuthenticated(name string) error {
+func (a *accountFeature) personShouldBeAuthenticated(name string) error {
 	return godog.ErrPending
 }
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
-	ctx.Step(`^(Bob|Tanya|Sue) has created an account$`, personHasCreatedAnAccount)
-	ctx.Step(`^(Bob|Tanya|Sue) has signed up$`, personHasSignedUp)
-	ctx.Step(`^(Bob|Tanya|Sue) should not be authenticated$`, personShouldNotBeAuthenticated)
-	ctx.Step(`^(Bob|Tanya|Sue) should not see any projects$`, personShouldNotSeeAnyProjects)
-	ctx.Step(`^(Bob|Tanya|Sue) should see an error telling (him|her|them) to activate the account$`, personShouldSeeAnErrorTellingThemToActivateTheAccount)
-	ctx.Step(`^(Bob|Tanya|Sue) tries to sign in$`, personTriesToSignIn)
-	ctx.Step(`^(Bob|Tanya|Sue) creates a project$`, personCreatesAProject)
-	ctx.Step(`^(Bob|Tanya|Sue) should see (his|her|the) project$`, personShouldSeeTheirProject)
-	ctx.Step(`^(Bob|Tanya|Sue) activates (his|her) account$`, personActivatesTheirAccount)
-	ctx.Step(`^(Bob|Tanya|Sue) should be authenticated$`, personShouldBeAuthenticated)
+	af := &accountFeature{}
+
+	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
+		return ctx, nil
+	})
+
+	ctx.Step(`^(Bob|Tanya|Sue) has created an account$`, af.personHasCreatedAnAccount)
+	ctx.Step(`^(Bob|Tanya|Sue) has signed up$`, af.personHasSignedUp)
+	ctx.Step(`^(Bob|Tanya|Sue) should not be authenticated$`, af.personShouldNotBeAuthenticated)
+	ctx.Step(`^(Bob|Tanya|Sue) should not see any projects$`, af.personShouldNotSeeAnyProjects)
+	ctx.Step(`^(Bob|Tanya|Sue) should see an error telling (him|her|them) to activate the account$`, af.personShouldSeeAnErrorTellingThemToActivateTheAccount)
+	ctx.Step(`^(Bob|Tanya|Sue) tries to sign in$`, af.personTriesToSignIn)
+	ctx.Step(`^(Bob|Tanya|Sue) creates a project$`, af.personCreatesAProject)
+	ctx.Step(`^(Bob|Tanya|Sue) should see (his|her|the) project$`, af.personShouldSeeTheirProject)
+	ctx.Step(`^(Bob|Tanya|Sue) activates (his|her) account$`, af.personActivatesTheirAccount)
+	ctx.Step(`^(Bob|Tanya|Sue) should be authenticated$`, af.personShouldBeAuthenticated)
 }
