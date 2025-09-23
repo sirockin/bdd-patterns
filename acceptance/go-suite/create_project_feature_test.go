e075609fa991fec056277b47a9202f62d6066b0d
Dave Sirockin <dave@sirockin.com>
2025-09-23 06:20:41 +0200

    refactor: rearrange steps in features for better readability

--- acceptance/go-suite/create_project_feature_test.go
+++ acceptance/go-suite/create_project_feature_test.go
@@ -2,22 +2,17 @@ package features_test
 
 // TestCreateOneProject tests creating a single project
 func (s *FeatureSuite) TestCreateOneProject() {
-	s.given().
-		personHasSignedUp("Sue").
-		when().
-		personCreatesAProject("Sue").
-		then().
-		personShouldSeeTheirProject("Sue")
+	s.
+		given().personHasSignedUp("Sue").
+		when().personCreatesAProject("Sue").
+		then().personShouldSeeTheirProject("Sue")
 }
 
 // TestTryToSeeSomeoneElsesProject tests that users cannot see other users' projects
 func (s *FeatureSuite) TestTryToSeeSomeoneElsesProject() {
-	s.given().
-		personHasSignedUp("Sue").
-		and().
-		personHasSignedUp("Bob").
-		when().
-		personCreatesAProject("Sue").
-		then().
-		personShouldNotSeeAnyProjects("Bob")
+	s.
+		given().personHasSignedUp("Sue").
+		and().personHasSignedUp("Bob").
+		when().personCreatesAProject("Sue").
+		then().personShouldNotSeeAnyProjects("Bob")
 }
--- acceptance/go-suite/sign_up_feature_test.go
+++ acceptance/go-suite/sign_up_feature_test.go
@@ -2,22 +2,17 @@ package features_test
 
 // TestSuccessfulSignUp tests successful account activation and authentication
 func (s *FeatureSuite) TestSuccessfulSignUp() {
-	s.given().
-		personHasCreatedAnAccount("Tanya").
-		when().
-		personActivatesTheirAccount("Tanya").
-		then().
-		personShouldBeAuthenticated("Tanya")
+	s.
+		given().personHasCreatedAnAccount("Tanya").
+		when().personActivatesTheirAccount("Tanya").
+		then().personShouldBeAuthenticated("Tanya")
 }
 
 // TestTryToSignInWithoutActivatingAccount tests sign-in failure without account activation
 func (s *FeatureSuite) TestTryToSignInWithoutActivatingAccount() {
-	s.given().
-		personHasCreatedAnAccount("Bob").
-		when().
-		personTriesToSignIn("Bob").
-		then().
-		personShouldNotBeAuthenticated("Bob").
-		and().
-		personShouldSeeAnErrorTellingThemToActivateTheAccount("Bob")
+	s.
+		given().personHasCreatedAnAccount("Bob").
+		when().personTriesToSignIn("Bob").
+		then().personShouldNotBeAuthenticated("Bob").
+		and().personShouldSeeAnErrorTellingThemToActivateTheAccount("Bob")
 }
